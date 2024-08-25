d3cb8ce8f57ea908294f12f91ea005ca798578d9
rigra <kth5352@gmail.com>
2024-08-25 18:28:23 +0900

    api로 데이터 받아오기 성공!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState(null);
+
+  //위치 가져오기
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+        getWeatherByCurrentLocation(lat, lon);
+      },
+      (error) => {
+        setError("위치 정보를 가져오는데 실패했습니다.");
+      }
+    );
+  };
+
+  //api 가져오기
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_APIKEY}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+
+  return <div></div>;
 }
 
 export default App;
